3f67aa95072f07be5973ff2d36eb07d48667d5af
suehi <110624980+007ekho@users.noreply.github.com>
2024-02-09 09:07:23 +0000

    Add files via upload

---
+++ sig.py
@@ -0,0 +1,58 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain_community.vectorstores import Chroma
+from decouple import config
+
+
+from langchain.embeddings import OpenAIEmbeddings
+embedding=OpenAIEmbeddings(openai_api_key= "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")
+vectordb = Chroma(
+    persist_directory= "C:/Users/USER/Downloads/Retrival_methods/new_chroma_sop/db",
+    embedding_function=embedding,
+    
+)
+
+chroma_retriever = Chroma()
+from langchain.chat_models import ChatOpenAI
+
+llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613",openai_api_key= "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")
+
+from langchain.chains import RetrievalQA
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm= llm,
+    retriever= vectordb.as_retriever(),
+    return_source_documents=True,
+    chain_type="stuff"
+   )
+    
+
+
+def rag_func(question: str) ->str:
+    """
+    This function takes in user question or prompt and returns a response
+    :param: question: string valiue of the question or the prompt from the user
+    :returns: string value of the answer to the user question
+    """
+    response = qa_chain({"query": question})
+
+    return response
+
+
+# def process_llm_response(llm_response):
+#     return llm_response['result']
+#     for source in llm_response["source_documents"]:
+#         return source.metadata['source']
+#     # return llm_response['result']
+#     print('\n\nSources:')
+#     # for source in llm_response["source_documents"]:
+#     #     return source.metadata['source']
+
+
+def process_llm_response(llm_response):
+    result = llm_response['result']
+    for source in llm_response["source_documents"]:
+        return result, source.metadata['source']
+
---
+++ streamlit.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from sig import rag_func
+from sig import process_llm_response
+if "messages" not in st.session_state.keys():
+    st.session_state["messages"]=[
+        {"role":"assistant","content": "Hello there,how can I help you today."}
+    ]
+
+#display the messages
+if "messages" in st.session_state.keys():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+
+user_prompt = st.chat_input()
+
+if user_prompt is not None:
+    st.session_state.messages.append({
+        "role":"user",
+        "content": user_prompt})
+    
+
+    with st.chat_message("user"):
+        st.write(user_prompt)
+
+
+if st.session_state.messages[-1]["role"]!= "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking...."):
+            ai_response = rag_func(user_prompt)
+            llm_response = process_llm_response(ai_response)
+            st.write(llm_response)
+
+
+    new_ai_message = {"role": "assistant", "content": llm_response}
+    st.session_state.messages.append(new_ai_message)
+
+
+
